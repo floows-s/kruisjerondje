6b14e16125fb85ec21aed1c3f1ed8f9e01cfb6dd
ThePizzaMuncher <j.eppinga7509@gmail.com>
2022-02-01 12:05:41 +0100

    shit gefixt; meerdere beurten komen nog.

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -17,8 +17,8 @@
 
 
 //in de globale scope zo dat elke functie er makelijk bij kan.
-std::string player1 = "Player 1";
-std::string player2 = "Player 2";
+std::string speler1 = "Speler 1";
+std::string speler2 = "Speler 2";
 char array [3][3] {'.','.','.','.','.','.','.','.','.'};
 
 void raster(){
@@ -31,74 +31,71 @@ void raster(){
 	std::cout << " -----------\n";
 }
 
-void invoer (){
-int rij = 0;
-int colom = 0;
-//speler 1
-std::cout << player1 << " welke rij: ";
-std::cin >> rij;
-//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
-if(rij<1 || rij>3) {
-    while (std::cin.fail() || rij<1 || rij>3) {
-    std::cout << "!Vul een getal in tussen de 1 en 3!\n";
-    std::cout << player1 << " welke rij: ";
-    std::cin.clear();
-    std::cin.ignore(245,'n');
-    std::cin >> rij;
-    }
-}
-rij--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
-std::cin.ignore();
-std::cout << player1 << " welke colom: ";
-std::cin >> colom;
-//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
-if(colom<1 || colom>3) {
-    while (std::cin.fail() || colom<1 || colom>3) {
-    std::cout << "!Vul een getal in tussen de 1 en 3!\n";
-    std::cout << player1 << " welke colom: ";
-    std::cin.clear();
-    std::cin.ignore(245,'n');
-    std::cin >> colom;
-    }
-}
-colom--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
-std::cin.ignore();
-array[rij][colom] = 'X';
+void invoer ()
+{
+	int rij = 0;
+	int kolom = 0;
+	//speler 1
+rijvraag1:
+	std::cout << speler1 << " welke rij: ";
+	std::cin >> rij;
+	//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
+	if(rij<1 || rij>3)
+	{
+		std::cout << "!Kies 1 van de 3 rijen!\n";
+		std::cin.clear();
+		std::cin.ignore();
+		goto rijvraag1;
+	}
+	rij--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
+	std::cin.ignore();
+kolomvraag1:
+	std::cout << speler1 << " welke kolom: ";
+	std::cin >> kolom;
+	//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
+	if(kolom<1 || kolom>3)
+	{
+		std::cout << "!Kies 1 van de 3 kolommen!\n";
+		std::cin.clear();
+		std::cin.ignore();
+		goto kolomvraag1;
+	}
+	kolom--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
+	std::cin.ignore();
+	array[rij][kolom] = 'X';
 
-//raster laten zien om te kijken wat speler 1 heeft gedaan
-system("CLS");
-raster();
+	//raster laten zien om te kijken wat speler 1 heeft gedaan
+	system("CLS");
+	raster();
 
-//speler 2
-std::cout << player2 << " welke rij: ";
-std::cin >> rij;
-//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
-if(rij<1 || rij>3) {
-    while (std::cin.fail() || rij<1 || rij>3) {
-    std::cout << "!Vul een getal in tussen de 1 en 3!\n";
-    std::cout << player2 << " welke rij: ";
-    std::cin.clear();
-    std::cin.ignore(245,'n');
-    std::cin >> rij;
-    }
-}
-rij--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
-std::cin.ignore();
-std::cout << player2 << " welke colom: ";
-std::cin >> colom;
-//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
-if(colom<1 || colom>3) {
-    while (std::cin.fail() || colom<1 || colom>3) {
-    std::cout << "!Vul een getal in tussen de 1 en 3!\n";
-    std::cout << player2 << " welke colom: ";
-    std::cin.clear();
-    std::cin.ignore(245,'n');
-    std::cin >> colom;
-    }
-}
-colom--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
-std::cin.ignore();
-array[rij][colom] = 'O';
+	//speler 2
+rijvraag2:
+	std::cout << speler2 << " welke rij: ";
+	std::cin >> rij;
+	//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
+	if(rij<1 || rij>3)
+	{
+		std::cout << "!Kies 1 van de 3 rijen!\n";
+		std::cin.clear();
+		std::cin.ignore();
+		goto rijvraag2;
+	}
+	rij--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
+	std::cin.ignore();
+kolomvraag2:	
+	std::cout << speler2 << " welke colom: ";
+	std::cin >> kolom;
+	//waneer er minder dan 1 of meer dan 3 wordt ingevoerd
+	if(kolom<1 || kolom>3)
+	{
+		std::cout << "!Kies 1 van de 3 kolommen!\n";
+		std::cin.clear();
+		std::cin.ignore();
+		goto kolomvraag2;
+	}
+	kolom--; //de array begint met tellen bij 0, waar dus rij 1 word ingevuld.
+	std::cin.ignore();
+	array[rij][kolom] = 'O';
 }
 
 
@@ -107,10 +104,10 @@ int main()
 {
 	//namen in vullen van de spelers
 	std::cout << "Wat is de naam van speler 1: ";
-	std::cin >> player1;
+	std::cin >> speler1;
 	std::cin.ignore();
 	std::cout << "Wat is de naam van speler 2: ";
-	std::cin >> player2;
+	std::cin >> speler2;
 	std::cin.ignore();
 	system("CLS");
 
